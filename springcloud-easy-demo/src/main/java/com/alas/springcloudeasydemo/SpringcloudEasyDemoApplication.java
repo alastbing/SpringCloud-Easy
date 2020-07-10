75aa0164aee6b780cfd4079b719e001e12b4401d
taobb <hi_tbing@163.com>
2020-07-10 15:04:19 +0800

    初始化demo

---
+++ springcloud-easy-demo/src/main/java/com/alas/springcloudeasydemo/SpringcloudEasyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alas.springcloudeasydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudEasyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudEasyDemoApplication.class, args);
+    }
+
+}
---
+++ springcloud-easy-demo/src/test/java/com/alas/springcloudeasydemo/SpringcloudEasyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alas.springcloudeasydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudEasyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
