5f7975097749407f6cbcedb6d3a0651c33b32ea9
taobb <hi_tbing@163.com>
2020-07-10 16:59:54 +0800

    api初始化

---
+++ springcloud-easy-api/src/main/java/com/alas/springcloudeasyapi/SpringcloudEasyApiApplication.java
@@ -0,0 +1,13 @@
+package com.alas.springcloudeasyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudEasyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudEasyApiApplication.class, args);
+    }
+
+}
