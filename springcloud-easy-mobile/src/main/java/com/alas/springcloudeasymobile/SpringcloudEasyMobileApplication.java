e772501d083d964714f7a93129d7d638992e15c4
taobb <hi_tbing@163.com>
2020-07-11 10:05:39 +0800

    SpringCloud-EASY-Mobile

---
+++ springcloud-easy-mobile/src/main/java/com/alas/springcloudeasymobile/SpringcloudEasyMobileApplication.java
@@ -0,0 +1,13 @@
+package com.alas.springcloudeasymobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudEasyMobileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudEasyMobileApplication.class, args);
+    }
+
+}
---
+++ springcloud-easy-mobile/src/test/java/com/alas/springcloudeasymobile/SpringcloudEasyMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alas.springcloudeasymobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudEasyMobileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
