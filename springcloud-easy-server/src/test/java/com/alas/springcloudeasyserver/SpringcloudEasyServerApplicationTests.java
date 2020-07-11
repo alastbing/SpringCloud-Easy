d5417ba888b88005307b04eb03876a603b1fe669
taobb <hi_tbing@163.com>
2020-07-11 09:53:49 +0800

    server

---
+++ springcloud-easy-server/src/test/java/com/alas/springcloudeasyserver/SpringcloudEasyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alas.springcloudeasyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudEasyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
