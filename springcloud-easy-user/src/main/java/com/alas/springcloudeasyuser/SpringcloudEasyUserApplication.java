69a97de7b452002d8bb8b7ceec221216af6855fb
bin <alastbing@163.com>
2020-07-13 16:57:04 +0800

    新增用户中心

---
+++ springcloud-easy-user/src/main/java/com/alas/springcloudeasyuser/SpringcloudEasyUserApplication.java
@@ -0,0 +1,13 @@
+package com.alas.springcloudeasyuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudEasyUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudEasyUserApplication.class, args);
+    }
+
+}
---
+++ springcloud-easy-user/src/test/java/com/alas/springcloudeasyuser/SpringcloudEasyUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alas.springcloudeasyuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudEasyUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
